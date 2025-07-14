2a8354c4a60ef54db187db2676a06822b1d3159d
KalaiselviiR <154441908+KalaiselviiR@users.noreply.github.com>
2025-07-14 22:39:07 +0530

    Add new changes to main branch

--- server.js
+++ server.js
@@ -16,4 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
+  });
+  
